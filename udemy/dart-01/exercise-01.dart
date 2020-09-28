e224a4e5aa9cbc962fe558bfa6608495dc0d3b8d
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-27 21:39:38 -0300

    [Exercise] Printing variables.

---
+++ udemy/dart-01/exercise-01.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = 'Marcelo';
+  String lastName = 'dos Santos';
+  int age = 36;
+  double height = 1.71;
+
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
