bc484af94ba82fde55a7a765e4a8cd1b3168750d
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-28 21:00:37 -0300

    [Exercise] String interpolation.

---
+++ udemy/dart-01/exercise-02.dart
@@ -0,0 +1,10 @@
+void main() {
+  double temperature = 20;
+  int value = 2;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  print("The temperature is ${temperature}C");
+  print("$value plus $value makes ${value + value}");
+  print("I like $pizza and $pasta");
+}
