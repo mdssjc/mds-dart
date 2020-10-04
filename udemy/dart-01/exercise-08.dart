e16a4e09676d5013be7f3b5fc9f4d2e2bbd7aec1
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-04 14:43:21 -0300

    [Exercise] Simple Calculator.

---
+++ udemy/dart-01/exercise-08.dart
@@ -0,0 +1,23 @@
+enum Operation { plus, minus, multiply, divide }
+
+void main() {
+  const a = 6;
+  const b = 3;
+  const op = Operation.divide;
+
+  var result;
+  switch (op) {
+    case Operation.plus:
+      print('$a + $b = ${a + b}');
+      break;
+    case Operation.minus:
+      print('$a - $b = ${a - b}');
+      break;
+    case Operation.multiply:
+      print('$a * $b = ${a * b}');
+      break;
+    case Operation.divide:
+      print('$a / $b = ${a / b}');
+      break;
+  }
+}
