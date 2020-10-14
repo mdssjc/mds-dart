7055dc055c160f64a8914bd19a5a54a4f727a0aa
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-13 22:06:37 -0300

    Parsing command line arguments.

---
+++ udemy/dart-01/projects/data_processing/totals.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Usage: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+
+  final inputFile = arguments.first;
+  print(inputFile);
+}
