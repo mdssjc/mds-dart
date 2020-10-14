dcc28dfa748e9576a8af482d67f868c2b0a1dc5f
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-13 22:21:14 -0300

    Implementing the processing logic.

--- udemy/dart-01/projects/data_processing/totals.dart
+++ udemy/dart-01/projects/data_processing/totals.dart
@@ -8,7 +8,26 @@ void main(List<String> arguments) {
 
   final inputFile = arguments.first;
   final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
+  var totalDuration = 0.0;
   for (var line in lines) {
-    print(line);
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+    totalDuration += duration;
   }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
+  print('Total for all tags: ${totalDuration.toStringAsFixed(1)}h');
 }
