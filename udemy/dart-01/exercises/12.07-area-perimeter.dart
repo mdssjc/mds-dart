7cd7ff30869330fa2346e465d56e145c86c49567
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-26 22:03:48 -0300

    Exercise: Area and Perimeter.

---
+++ udemy/dart-01/exercises/12.07-area-perimeter.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+abstract class Shape {
+  double get area;
+
+  double get perimeter;
+
+  void printValues() {
+    print('area: $area, perimeter: $perimeter');
+  }
+}
+
+class Square extends Shape {
+  Square(this.side);
+
+  final double side;
+
+  @override
+  double get area => side * side;
+
+  @override
+  double get perimeter => 4 * side;
+}
+
+class Circle extends Shape {
+  Circle(this.radius);
+
+  final double radius;
+
+  @override
+  double get area => pi * radius * radius;
+
+  @override
+  double get perimeter => 2 * pi * radius;
+}
+
+void main() {
+  var shapes = [
+    Square(5),
+    Circle(4),
+  ];
+  shapes.forEach((shape) => shape.printValues());
+}
