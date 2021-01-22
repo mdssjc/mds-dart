c735cf9b3fff441890ad638e97e84136e510e2c1
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-21 22:12:15 -0300

    Exercise: Email validation.

---
+++ udemy/dart-01/exercises/15.05-email-format-exception.dart
@@ -0,0 +1,28 @@
+class EmailAddress {
+  final String email;
+
+  EmailAddress(this.email) {
+    if (email.isEmpty) {
+      throw FormatException('email can\'t be empty');
+    }
+    if (!email.contains('@')) {
+      throw FormatException('$email doesn\'t contain the @ symbol');
+    }
+  }
+
+  @override
+  String toString() => email;
+}
+
+void main() {
+  try {
+    print(EmailAddress('me@example.com'));
+    print(EmailAddress('example.com'));
+    print(EmailAddress(''));
+  } on FormatException catch (e) {
+    print(e);
+  }
+  // Desired output:
+  // me@example.com
+  // FormatException: example.com doesn't contain the @ symbol
+}
