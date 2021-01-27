cdb7ce3a58158a3b7a5f11e31e070d3d2e75fc4f
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-26 22:51:18 -0300

    async and await.

--- udemy/dart-01/lessons/async_programming/futures.dart
+++ udemy/dart-01/lessons/async_programming/futures.dart
@@ -1,13 +1,22 @@
 Future<String> fetchUserOrder() => Future.delayed(
       Duration(seconds: 2),
-      () => 'Cappuccino',
-      // () => throw Exception('Out of milk'),
+      // () => 'Cappuccino',
+      () => throw Exception('Out of milk'),
     );
 
-void main() {
+Future<void> main() async {
   print('Program started');
   fetchUserOrder()
       .then((order) => print('Order is ready: $order'))
       .catchError((error) => print(error))
       .whenComplete(() => print('Done'));
+
+  try {
+    final order = await fetchUserOrder();
+    print(order);
+  } catch (e) {
+    print(e);
+  } finally {
+    print('Done');
+  }
 }
