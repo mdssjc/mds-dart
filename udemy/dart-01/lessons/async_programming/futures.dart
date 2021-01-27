9008c2fb0e4b70103b65ad6e8eaa441b548cf961
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-26 22:54:31 -0300

    Future.value and Future.error.

--- udemy/dart-01/lessons/async_programming/futures.dart
+++ udemy/dart-01/lessons/async_programming/futures.dart
@@ -1,8 +1,12 @@
 Future<String> fetchUserOrder() => Future.delayed(
       Duration(seconds: 2),
-      // () => 'Cappuccino',
-      () => throw Exception('Out of milk'),
+      () => 'Cappuccino',
+      // () => throw Exception('Out of milk'),
     );
+
+Future<String> fetchUserOrder2() => Future.value('Espresso');
+
+Future<String> fetchUserOrder3() => Future.error(UnimplementedError());
 
 Future<void> main() async {
   print('Program started');
@@ -14,6 +18,9 @@ Future<void> main() async {
   try {
     final order = await fetchUserOrder();
     print(order);
+    final order2 = await fetchUserOrder2();
+    print(order2);
+    // final order3 = await fetchUserOrder3();
   } catch (e) {
     print(e);
   } finally {
