e007ef744b9aef17033ff9c763cfede00cccd431
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-27 22:34:02 -0300

    Stream generators: async* and yield.

--- udemy/dart-01/lessons/async_programming/streams.dart
+++ udemy/dart-01/lessons/async_programming/streams.dart
@@ -9,10 +9,31 @@ Future<int> sumStream(Stream<int> stream) async {
 Future<int> sumStream2(Stream<int> stream) =>
     stream.reduce((previous, element) => previous + element);
 
+Stream<int> countStream(int n) async* {
+  for (var i = 1; i <= n; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    print(i);
+    yield i;
+  }
+}
+
+Iterable<int> count(int n) sync* {
+  for (var i = 1; i <= n; i++) {
+    yield i;
+  }
+}
+
 Future<void> main() async {
   final stream = Stream.fromIterable([1, 2, 3, 4]);
+  final stream2 = countStream(4);
 
   // final sum = await sumStream(stream);
-  final sum = await sumStream2(stream);
+  var sum = await sumStream2(stream);
+  print('Sum: $sum');
+
+  sum = await sumStream2(stream2);
+  print('Sum: $sum');
+
+  sum = count(4).reduce((value, element) => value + element);
   print('Sum: $sum');
 }
