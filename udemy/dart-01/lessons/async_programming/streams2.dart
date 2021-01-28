ea31cbd0e5e93d01b2caf6a38e7a2420bb8dc7a7
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-27 22:50:00 -0300

    Stream methods.

--- udemy/dart-01/lessons/async_programming/streams2.dart
+++ udemy/dart-01/lessons/async_programming/streams2.dart
@@ -1,8 +1,15 @@
-void main() {
-  Stream.fromIterable([1, 2, 3]);
+Future<void> main() async {
+  final stream = Stream.fromIterable([1, 2, 3]);
   Stream.value(10);
   Stream.error(Exception('something went wrong'));
   Stream.empty();
   Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
   Stream.periodic(Duration(seconds: 1), (index) => index);
+
+  // final value = await stream.first;
+  // print(value);
+  // await stream.forEach((element) => print(element));
+  final doubles = stream.map((value) => value * 2).where((value) => value > 3);
+  // print(doubles);
+  await doubles.forEach(print);
 }
