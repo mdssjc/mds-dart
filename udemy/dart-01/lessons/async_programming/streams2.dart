6bd2ce9a974c58dd9f6a18eec0b46d780e2809e2
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-27 22:45:31 -0300

    Stream constructors.

---
+++ udemy/dart-01/lessons/async_programming/streams2.dart
@@ -0,0 +1,8 @@
+void main() {
+  Stream.fromIterable([1, 2, 3]);
+  Stream.value(10);
+  Stream.error(Exception('something went wrong'));
+  Stream.empty();
+  Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
+  Stream.periodic(Duration(seconds: 1), (index) => index);
+}
