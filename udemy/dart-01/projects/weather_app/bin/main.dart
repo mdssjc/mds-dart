11d35d329c863caf1c0c23ea76257c0bdeb65a89
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-28 23:04:26 -0300

    Creating the command-line app.

---
+++ udemy/dart-01/projects/weather_app/bin/main.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  if (arguments.length != 1) {
+    print('Syntax: dart bin/main.dart <city>');
+    return;
+  }
+
+  final city = arguments.first;
+  print(city);
+}
