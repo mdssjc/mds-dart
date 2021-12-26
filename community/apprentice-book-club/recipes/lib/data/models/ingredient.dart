4f45f3f8a32718a32c2690a1ed7c0bcbed3b1d7d
Marcelo dos Santos <mdssjc@gmail.com>
2021-12-25 21:19:04 -0300

    feat: add Saving Data With SQLite

--- community/apprentice-book-club/recipes/lib/data/models/ingredient.dart
+++ community/apprentice-book-club/recipes/lib/data/models/ingredient.dart
@@ -11,4 +11,18 @@ class Ingredient extends Equatable {
 
   @override
   List<Object?> get props => [recipeId, name, weight];
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) => Ingredient(
+        id: json['ingredientId'],
+        recipeId: json['recipeId'],
+        name: json['name'],
+        weight: json['weight'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'ingredientId': id,
+        'recipeId': recipeId,
+        'name': name,
+        'weight': weight,
+      };
 }
--- community/apprentice-book-club/recipes/lib/main.dart
+++ community/apprentice-book-club/recipes/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
-import 'data/memory_repository.dart';
+// import 'data/memory_repository.dart';
+// import 'data/sqlite/sqlite_repository.dart';
+import 'data/moor/moor_repository.dart';
 import 'data/repository.dart';
 import 'network/recipe_service.dart';
 import 'network/service_interface.dart';
@@ -12,18 +14,25 @@ import 'ui/main_screen.dart';
 Future<void> main() async {
   _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  // final repository = SqliteRepository();
+  final repository = MoorRepository();
+  await repository.init();
+  runApp(MyApp(repository: repository));
 }
 
 void _setupLogging() {
   Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
+  Logger.root.onRecord.listen(
+    (rec) {
+      print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Repository repository;
+
+  const MyApp({Key? key, required this.repository}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +40,11 @@ class MyApp extends StatelessWidget {
       providers: [
         Provider<Repository>(
           lazy: false,
-          create: (_) => MemoryRepository(),
+          create: (_) => repository,
+          dispose: (_, Repository repository) => repository.close(),
         ),
         Provider<ServiceInterface>(
+          // create: (_) => MockService()..create(),
           create: (_) => RecipeService.create(),
           lazy: false,
         ),
