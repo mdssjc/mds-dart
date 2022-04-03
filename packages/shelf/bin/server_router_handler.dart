a9f3666125916ee3a5c308ca39b8da54ae13f461
Marcelo dos Santos <mdssjc@gmail.com>
2022-04-02 22:10:11 -0300

    feat: add shelf part 3

--- packages/shelf/bin/server_router_handler.dart
+++ packages/shelf/bin/server_router_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -6,7 +8,13 @@ class ServerRouterHandler {
     final router = Router();
 
     router.get('/', (Request req) {
-      return Response.ok('Primeira Rota!');
+      return Response(
+        200,
+        body: '<h1>Primeira Rota!</h1>',
+        headers: {
+          'content-type': 'text/html',
+        },
+      );
     });
 
     router.get('/ola/mundo/<user>', (Request request, String user) {
@@ -17,6 +25,29 @@ class ServerRouterHandler {
       String? name = req.url.queryParameters['name'];
       String? age = req.url.queryParameters['age'];
       return Response.ok('Query is: $name, age: $age');
+    });
+
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      var json = jsonDecode(result);
+
+      var username = json['username'];
+      var password = json['password'];
+
+      if (username == 'admin' && password == '123') {
+        var jsonResponse = jsonEncode({
+          'token': 'token123',
+          'user_id': 1,
+        });
+
+        return Response.ok(
+          jsonResponse,
+          headers: {
+            'content-type': 'application/json',
+          },
+        );
+      }
+      return Response.forbidden('Forbidden');
     });
 
     return router;
