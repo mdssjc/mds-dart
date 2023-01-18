6cb784bccb3a03e6a25c8a45dbc35cb3ce4fa677
Marcelo dos Santos <mdssjc@gmail.com>
2023-01-18 19:30:52 -0300

    feat: add Using FFI in a Flutter plugin

--- codelabs/flutter-ffigen/lib/flutter_ffigen.dart
+++ codelabs/flutter-ffigen/lib/flutter_ffigen.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:ffi/ffi.dart' as ffi;
+import 'package:path/path.dart' as p;
 
 import 'duktape_bindings_generated.dart';
 
@@ -9,12 +10,24 @@ const String _libName = 'flutter_ffigen';
 
 final DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open('build/macos/Build/Products/Debug'
+          '/$_libName/$_libName.framework/$_libName');
+    }
     return DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open(
+          'build/linux/x64/debug/bundle/lib/lib$_libName.so');
+    }
     return DynamicLibrary.open('lib$_libName.so');
   }
   if (Platform.isWindows) {
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open(p.canonicalize(
+          p.join(r'build\windows\runner\Debug', '$_libName.dll')));
+    }
     return DynamicLibrary.open('$_libName.dll');
   }
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
@@ -28,9 +41,9 @@ class Duktape {
         _bindings.duk_create_heap(nullptr, nullptr, nullptr, nullptr, nullptr);
   }
 
-  void evalString(String jsCode) {
+  String evalString(String jsCode) {
     var nativeUtf8 = jsCode.toNativeUtf8();
-    _bindings.duk_eval_raw(
+    final evalResult = _bindings.duk_eval_raw(
         ctx,
         nativeUtf8.cast<Char>(),
         0,
@@ -41,10 +54,21 @@ class Duktape {
             DUK_COMPILE_STRLEN |
             DUK_COMPILE_NOFILENAME);
     ffi.malloc.free(nativeUtf8);
+
+    if (evalResult != 0) {
+      throw _retrieveTopOfStackAsString();
+    }
+
+    return _retrieveTopOfStackAsString();
   }
 
-  int getInt(int index) {
-    return _bindings.duk_get_int(ctx, index);
+  String _retrieveTopOfStackAsString() {
+    Pointer<Size> outLengthPtr = ffi.calloc<Size>();
+    final errorStrPtr = _bindings.duk_safe_to_lstring(ctx, -1, outLengthPtr);
+    final returnVal =
+        errorStrPtr.cast<ffi.Utf8>().toDartString(length: outLengthPtr.value);
+    ffi.calloc.free(outLengthPtr);
+    return returnVal;
   }
 
   void dispose() {
